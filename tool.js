3d32f6db67cf85a158c4eeb28c83520f612cfe20
Konstantin Makarychev <mrsimpson@inbox.ru>
2014-09-29 22:00:50 +0400

    fix `MissingRequiredParameter` error

--- tool.js
+++ tool.js
@@ -32,9 +32,9 @@ module.exports = function(options) {
                 }
 
                 // Causing problems on a default cloudfront setup, why is this needed?
-                // if (data.DistributionConfig.Origins.Items[0].S3OriginConfig.OriginAccessIdentity === null) {
-                //   data.DistributionConfig.Origins.Items[0].S3OriginConfig.OriginAccessIdentity = '';
-                // }
+                if (data.DistributionConfig.Origins.Items[0].S3OriginConfig.OriginAccessIdentity === null) {
+                   data.DistributionConfig.Origins.Items[0].S3OriginConfig.OriginAccessIdentity = '';
+                }
 
                 if (data.DistributionConfig.DefaultRootObject === defaultRootObject.substr(1)) {
                     gutil.log('gulp-cloudfront:', "DefaultRootObject hasn't changed, not updating.");
